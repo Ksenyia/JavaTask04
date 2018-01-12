f3b14e00768b07c3a1d680ae262213639e20f300
Ksenyia <ksenea1@gmail.com>
2018-01-12 03:01:13 +0300

    Update Controller.java

--- SimpleWebApp02/src/main/java/by/tr/web/controller/Controller.java
+++ SimpleWebApp02/src/main/java/by/tr/web/controller/Controller.java
@@ -29,36 +29,36 @@ public class Controller extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int recordsPerPage = 5;
-        int firstPage = 1;
-        double convertIntToDouble = 1.0;
+        	int firstPage = 1;
+        	double convertIntToDouble = 1.0;
 		String massage = "do Post";
 		log.info(massage); 
 		String requestAttribute = "naming";
 		String jspFile = "/information.jsp";
-        HttpSession httpSession = request.getSession();
+        	HttpSession httpSession = request.getSession();
 		String submitName = "parser";
 		String parser = request.getParameter(submitName);
-    	Service service = new ServiceImpl();
-    	List<Flower> orangery = null;
-        try {
-        	orangery = service.parse(parser);
-        } catch (SAXException e) {
-        	log.error(e.getMessage());
+		Service service = new ServiceImpl();
+		List<Flower> orangery = null;
+		try {
+			orangery = service.parse(parser);
+		} catch (SAXException e) {
+			log.error(e.getMessage());
 		}
-        int countOfRecords = orangery.size();
-        String recordsPerPageAttr = "recordsPerPage";
+		int countOfRecords = orangery.size();
+		String recordsPerPageAttr = "recordsPerPage";
 		httpSession.setAttribute(recordsPerPageAttr, recordsPerPage);
-        String countOfRecordsAttr = "countOfRecords";
+        	String countOfRecordsAttr = "countOfRecords";
 		httpSession.setAttribute(countOfRecordsAttr, countOfRecords);
-        int countOfPages = (int) Math.ceil(countOfRecords * convertIntToDouble / recordsPerPage);
-        String countOfPageAttr = "countOfPages";
+        	int countOfPages = (int) Math.ceil(countOfRecords * convertIntToDouble / recordsPerPage);
+        	String countOfPageAttr = "countOfPages";
 		httpSession.setAttribute(countOfPageAttr, countOfPages);
-        String currentPageAttr = "currentPage";
+        	String currentPageAttr = "currentPage";
 		httpSession.setAttribute(currentPageAttr,firstPage);
-	    Object obj = orangery;
-	    httpSession.setAttribute(requestAttribute, obj);
+	    	Object obj = orangery;
+	    	httpSession.setAttribute(requestAttribute, obj);
 		RequestDispatcher dispatcher = request.getRequestDispatcher(jspFile);
-	    dispatcher.include(request, response);
+	    	dispatcher.include(request, response);
 	}
 
 }
